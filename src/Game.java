bf4ed3984624a3d8c3cd8bc0024feb62dc491b76
amaysrinivasan <amay.srinivasan@menloschool.org>
2024-11-22 13:44:39 -0800

    Worked on game a little

--- src/Game.java
+++ src/Game.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
 public class Game {
     private Player player;
     private Deck deck;
 
-    public Game(Player player, Deck deck) {
-        this.player = player;
-        this.deck = deck;
-    }
-    public static void main(String[] args) {
+    public Game() {
         String suits[] = {"Hearts", "Clubs", "Diamonds", "Spades"};
         String ranks[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
-        int values[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11, 11};
+        int values[] = {11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 11, 11};
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = input.nextLine()
+        deck = new Deck(ranks, suits, values);
+        player = new Player(name);
+
+    }
+    public static void main(String[] args) {
+
+
 
 
     }
